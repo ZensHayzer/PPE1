d6476302d526ae846781d11d857e99ac01a09af7
ZensHayzer <alexandre.jeanne10@gmail.com>
2018-04-13 14:18:47 +0200

    modified:   css/style.css 	modified:   vues/documents.php 	php/moteurRecherche.php 	vues/mentions.php

--- vues/documents.php
+++ vues/documents.php
@@ -1,17 +1,16 @@
 <?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=ppe1;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
-}
 
-
-$reponse = $bdd->query('SELECT * FROM auteur');
-$livre = $bdd->prepare('SELECT * FROM document WHERE ID_Auteur = ?');
+echo '<form method="POST" action="php/moteurRecherche.php">
+		<input type="search" name="filtre">
+		<label for="typeFiltre">Choisir un type de filtre : </label>
+		<select name="typeFiltre">
+			<option value="nom">Nom</option>
+			<option value="prenom">Prénom</option>
+			<option value="titre">Titre</option>
+			<option value="date">Date</option>
+		</select>
+		<input type="submit">
+	  </form>';
 
 echo '<div >';
 echo '<table id="gestDocTab">';
@@ -22,21 +21,9 @@ echo '  	<tr>
             <td><p>Date</p></td>            
         </tr>';
 		
-while ($donnees = $reponse->fetch())
-{
-		$livre -> execute(array($donnees['ID_Auteur']));
-		$donneesLivre = $livre -> fetch();
-		
-        echo '<tr>';
-		echo '<td>'.$donnees['Nom_Auteur'].'</td>';
-		echo '<td>'.$donnees['Prenom_Auteur'].'</td>';
-		echo '<td>'.$donneesLivre['Titre'].'</td>';
-		echo '<td>'.$donneesLivre['Date'].'</td>';
-		echo '</tr>';
-}
+
 
 echo '</table>';
 echo '</div>';
-$reponse->closeCursor(); // Termine le traitement de la requête
 
 ?>
